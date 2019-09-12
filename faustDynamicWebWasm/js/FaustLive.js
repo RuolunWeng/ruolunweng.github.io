b1108054b4501301fe39ffe6d411076ca5bf537b
Allen Weng <allen1991shcm@gmail.com>
2019-09-12 10:34:08 +0800

    FAUST WEB test

--- faustDynamicWebWasm/js/FaustLive.js
+++ faustDynamicWebWasm/js/FaustLive.js
@@ -21,8 +21,8 @@ var ftz_flag = "2";
 var poly_nvoices = 16;
 var output_handler = null;
 var save_state = true;
-var libraries_url = "http://192.168.1.102:8000/libraries/";
-//var libraries_url = "http://faust.grame.fr/libraries/";
+//var libraries_url = "http://192.168.1.102:8000/libraries/";
+var libraries_url = "http://allenweng.000webhostapp.com/faustDynamicWebWasm/libraries/";
 var module = null;
 
 
